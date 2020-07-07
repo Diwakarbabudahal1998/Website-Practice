0ac501bb2ed2d034217889d59d1b4ec9ad5385fa
Diwakar <diwakardahal66@gmail.com>
2020-07-08 00:27:19 +0545

    website complete

--- js/main.js
+++ js/main.js
@@ -7,6 +7,21 @@ toggleCollapse.onclick = () => {
     nav.classList.toggle("collapse");
     //e.target.classList.toggle("toggle-click");
 } */
+const responsive={
+    0:{
+        items:1
+    },
+    320:{
+        items:1
+    },
+    560:{
+        items:2
+    },
+    960:{
+        items:3
+    }
+
+}
 $(document).ready(function(){
     $nav=$('.nav')
     $toggleCollapse=$('.toggle-collapse');
@@ -21,9 +36,15 @@ $(document).ready(function(){
         autoplayTimeout:3000,
         dots:false,
         nav:true,
-        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')]
-        
+        navText:[$('.owl-navigation .owl-nav-prev'),$('.owl-navigation .owl-nav-next')],
+        responsive:responsive
     });
+    //click to scroll top
+    $('.move-up span').click(function(){
+        $('html,body').animate({
+            scrollTop:0,
+        },1000);
+    })
 
-})
+});
 
